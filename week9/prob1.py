109c00d882165d0a74ac536895b69b3b29af4a4e
kimjinh112 <kimjinh112@nate.com>
2018-10-30 14:12:59 +0900

    week9
    dd

--- week9/prob1.py
+++
@@ -1,8 +0,0 @@
-def is_odd(num):
-    if num % 2 == 1:
-        return True
-    else:
-        return False
-
-print(is_odd(8))
-print(is_odd(3))
--- week9/prob2.py
+++
@@ -1,7 +0,0 @@
-def attend(names):
-    for m in member:
-        print('출석,', m)
-
-
-member = ['길동', '춘향', '세종', '지용', '민호', '재현']
-attend(member)
